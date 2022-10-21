1dd584cb2a6b2d764e992971626419d3d63f5144
Tanjid Hossain <tanjidhossain@gmail.com>
2022-10-21 00:45:07 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+    res.send("Route is working!");
+});
+
+// app.use('/api/v1/tours', tourRoute)
+
+module.exports = app;
